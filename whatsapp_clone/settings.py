ab4cf90f7cf0b00b9ff07261b511c4369c9e6ee0
SamEntraiot <samu16082001@gmail.com>
2025-09-19 10:03:11 +0530

    update hosts

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["samu2001.pythonanywhere.com"]
 
 
 # Application definition
