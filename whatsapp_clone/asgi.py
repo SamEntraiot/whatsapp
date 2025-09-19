b48603345f58138eb8f50da034d7c85c020bc5bd
SamEntraiot <samu16082001@gmail.com>
2025-09-19 17:16:19 +0530

    render hosts fixing

--- whatsapp_clone/asgi.py
+++ whatsapp_clone/asgi.py
@@ -9,14 +9,23 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat.routing
 
+# Set the settings module environment variable.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_clone.settings')
 
+# Initialize the Django ASGI application early to ensure settings are configured.
+django_asgi_app = get_asgi_application()
+
+# Now, import the Channels components.
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    # Django's ASGI application to handle traditional HTTP requests.
+    "http": django_asgi_app,
+
+    # WebSocket chat handler.
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
